d90383d94c2b6caa6cd393d4cf99071d9156dbc9
Shatgul Ogen <shatgul@Shatguls-MBP.lan>
2022-11-17 10:59:20 -0500

    edit the layout so mac and android now both have nice safeareaview layout

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from "react";
+import { StatusBar, SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: 'center'}}>
-      <Text>My react native blank canvas</Text>
+    <>
+    <SafeAreaView style={{flex:1, marginTop: StatusBar.currentHeight}}>
+    <View style={{padding: 16, backgroundColor: "pink",}}>
+      <Text>search</Text>
     </View>
+    <View style={{flex: 1, padding: 16, backgroundColor: "lightblue",}}>
+      <Text>list</Text>
+    </View>
+    </SafeAreaView>
+    <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({ 
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
